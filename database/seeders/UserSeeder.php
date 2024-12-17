6601ee4f0c848e882b730d0058a4468d5586bf8f
usirgeoffery7755 <usirgeoffery@travimobile.com>
2024-12-17 10:48:08 +0100

    added details

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,7 +14,7 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         DB::table('users')->insert([
-            ['uuid' => '6983571734087474', 'first_name' => 'Admin', 'last_name' => 'Administrator', 'phone_number' => '012345678910', 'email' => 'admin@admin.com', 'password' => '$2y$10$C8gijn/TMfiFqepFvVIldOTyEQ5K9lf3ZJoNaUhotkYdKbZ23pDyy', 'verification_code' => '09458']
+            ['uuid' => '6983571734087474', 'first_name' => 'Admin', 'last_name' => 'Administrator', 'phone_number' => '012345678910', 'email' => 'admin@travimobile.com', 'password' => '$2y$10$C8gijn/TMfiFqepFvVIldOTyEQ5K9lf3ZJoNaUhotkYdKbZ23pDyy', 'verification_code' => '09458']
         ]);
     }
 }
