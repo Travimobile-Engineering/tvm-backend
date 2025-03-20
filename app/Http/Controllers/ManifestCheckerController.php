f6ae902b7bcd8d40b5b83940bbc9e0b3367976e1
onyiimesi <onyedikachukwu62@gmail.com>
2025-03-20 08:53:49 +0100

    Added fixes

--- app/Http/Controllers/ManifestCheckerController.php
+++ app/Http/Controllers/ManifestCheckerController.php
@@ -9,29 +9,35 @@ use Illuminate\Http\Request;
 
 class ManifestCheckerController extends Controller
 {
-    protected $service;
-
-    public function __construct(){
-        $this->service = new ManifestCheckerService();
-    }
     use HttpResponse;
-    public function getManifestData(Request $request){
+
+    public function __construct(
+        protected ManifestCheckerService $service
+    )
+    {}
+
+    public function getManifestData(Request $request)
+    {
         return $this->service->getManifestData($request);
     }
 
-    public function addIncident(AddIncidentRequest $request){
+    public function addIncident(AddIncidentRequest $request)
+    {
         return $this->service->addIncident($request);
     }
 
-    public function getIncidentCategories(){
+    public function getIncidentCategories()
+    {
         return $this->service->getIncidentCategories();
     }
 
-    public function getIncidentTypes(){
+    public function getIncidentTypes()
+    {
         return $this->service->getIncidentTypes();
     }
 
-    public function getIncidentSeverityLevels(){
+    public function getIncidentSeverityLevels()
+    {
         return $this->service->getIncidentSeverityLevels();
     }
 }
