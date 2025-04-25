79f3d3835c91d61d866b7f79e9c069a2bc87b962
Stephen <bsysteve@gmail.com>
2025-04-25 10:41:52 +0100

    modify job openings table

---
+++ database/migrations/2025_04_25_103306_modify_job_openings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('job_openings', function(Blueprint $table){
+            $table->text('summary')->change();
+            $table->longText('responsibilities')->change();
+            $table->text('requirement')->change();
+            $table->tinyText('offer')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('job_openings', function(Blueprint $table){
+
+            $table->tinyText('summary')->change();
+            $table->text('responsibilities')->change();
+            $table->tinyText('requirement')->change();
+            $table->string('offer')->change();
+        });
+    }
+};
