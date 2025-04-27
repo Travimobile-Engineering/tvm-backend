fdc10cd07ff8beca984c35d7ae967de7b6cefd3a
onyiimesi <onyedikachukwu62@gmail.com>
2025-04-27 14:43:21 +0100

    Trip tests

---
+++ database/factories/Vehicle/VehicleFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories\Vehicle;
+
+use App\Models\TransitCompany;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Vehicle\Vehicle>
+ */
+class VehicleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word,
+            'company_id' => TransitCompany::factory()->create()->id,
+            'user_id' => User::factory()->create()->id,
+            'brand_id' => $this->faker->randomElement([1, 2, 3]),
+            'ac' => $this->faker->boolean,
+            'plate_no' => $this->faker->unique()->word,
+            'color' => $this->faker->colorName,
+            'model' => $this->faker->word,
+            'air_conditioned' => $this->faker->boolean,
+            'seats' => $this->faker->numberBetween(10, 50),
+        ];
+    }
+}
