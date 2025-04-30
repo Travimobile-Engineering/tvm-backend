b2a3900b99a17daf12c8220a69a18f84420a45f5
Stephen <bsysteve@gmail.com>
2025-04-30 15:18:08 +0100

    fix job application migration issue

--- database/migrations/2025_04_30_122504_modify_job_application_status_column.php
+++ database/migrations/2025_04_30_122504_modify_job_application_status_column.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\JobApplicationStatusEnum;
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Enum\JobApplicationStatusEnum;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
