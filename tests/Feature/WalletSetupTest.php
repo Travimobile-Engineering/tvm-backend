46e613981402e9f8628c47f57d54c208116c7ee6
Stephen <bsysteve@gmail.com>
2025-05-07 11:56:22 +0100

    wallet setup test

---
+++ tests/Feature/WalletSetupTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Bank;
+use App\Models\User;
+use Database\Seeders\BankSeeder;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class WalletSetupTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $headers, $user, $token;
+    public function setUp() :void{
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+        $this->token = JWTAuth::fromUser($this->user);
+        
+        $this->headers = [
+            'Accept' => 'application/json',
+            config('security.header_key') => config('security.header_value'),
+            'Authorization' => 'Bearer '.$this->token,
+        ];
+
+        (new BankSeeder())->run();
+    }
+    public function test_walletSetup(): void
+    {
+        $payload = [
+            'user_id' => $this->user->id,
+            'bank_name' => 'Access Bank',
+            'account_number' => '0123456789',
+            'account_name' => 'Test Account',
+            'pin' => '1234',
+            'pin_confirmation' => '1234'
+        ];
+        $response = $this->postJson('api/user/wallet/setup', $payload, $this->headers);
+        
+        $this->assertDatabaseHas('user_banks', [
+            'bank_name' => $payload['bank_name'],
+            'account_number' => $payload['account_number'],
+            'account_name' => $payload['account_name'],
+        ]);
+
+        $this->assertDatabaseCount('user_pins', 1);
+        $response->assertStatus(201);
+    }
+}
