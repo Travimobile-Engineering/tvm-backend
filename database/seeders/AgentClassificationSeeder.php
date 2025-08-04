69bd24307cdc7e3ccc65d56031dddc9b1ae3958c
onyiimesi <onyedikachukwu62@gmail.com>
2025-08-04 09:37:50 +0100

    Added new column

--- database/seeders/AgentClassificationSeeder.php
+++ database/seeders/AgentClassificationSeeder.php
@@ -14,10 +14,10 @@ class AgentClassificationSeeder extends Seeder
     public function run(): void
     {
         $data = [
-            ['level' => 'D', 'amount' => 1000],
-            ['level' => 'C', 'amount' => 10000],
-            ['level' => 'B', 'amount' => 100000],
-            ['level' => 'A', 'amount' => 1000000],
+            ['level' => 'D', 'amount' => 1000, 'reward_amount' => 100],
+            ['level' => 'C', 'amount' => 10000, 'reward_amount' => 1000],
+            ['level' => 'B', 'amount' => 100000, 'reward_amount' => 10000],
+            ['level' => 'A', 'amount' => 1000000, 'reward_amount' => 100000],
         ];
 
         DB::table('agent_classifications')->insert($data);
