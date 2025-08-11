9cf4e67ba18012d5d047de21a4f0895dccce5efd
onyiimesi <onyedikachukwu62@gmail.com>
2025-08-11 13:31:32 +0100

    Fix

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -2,15 +2,32 @@
 
 namespace App\Http\Controllers;
 
-use App\Trait\HttpResponse;
 use App\Models\Notification;
+use App\Trait\HttpResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use App\Services\NotificationService;
 
 class NotificationController extends Controller
 {
     use HttpResponse;
-    public function all(){
-        return $this->response(['data' => Notification::where('user_id', Auth::id())->orderBy('created_at', 'desc')->get()]);
+
+    public function __construct(
+        protected NotificationService $notificationService
+    )
+    {}
+
+    public function all()
+    {
+        return $this->notificationService->all();
+    }
+
+    public function show(Notification $notification)
+    {
+        return $this->notificationService->show($notification);
+    }
+
+    public function delete(Notification $notification)
+    {
+        return $this->notificationService->delete($notification);
     }
 }
---
+++ app/Services/NotificationService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Services;
+
+use App\Trait\HttpResponse;
+use App\Models\Notification;
+use Illuminate\Support\Facades\Auth;
+
+class NotificationService
+{
+    use HttpResponse;
+
+    public function all()
+    {
+        $notifications = Notification::where('user_id', Auth::id())
+            ->latest()
+            ->paginate(25);
+
+        return $this->withPagination($notifications, "All Notifications");
+    }
+
+    public function show($notification)
+    {
+        return $this->success($notification, "Notification retrieved successfully");
+    }
+
+    public function delete($notification)
+    {
+        $notification->delete();
+        return $this->success(null, "Notification deleted successfully");
+    }
+}
+
