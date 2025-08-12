8e2e7313868cca5e70655b1a867e3f436801be33
onyiimesi <onyedikachukwu62@gmail.com>
2025-08-12 17:24:58 +0100

    Add new column to txn table and created new API

--- app/Services/ERP/ChargeService.php
+++ app/Services/ERP/ChargeService.php
@@ -35,7 +35,7 @@ class ChargeService
             app(AccountService::class)->initiateTransfer($feeAmount);
 
             $reference = generateReference('SMS', 'transactions');
-            $user->createTransaction(TransactionTitle::SMS_CHARGE->value, $feeAmount, 'DR', $reference, 'SMS charge');
+            $user->createTransaction(TransactionTitle::SMS_CHARGE->value, $feeAmount, 'DR', $reference, 'You have received an SMS charge.');
         });
     }
 
@@ -64,7 +64,7 @@ class ChargeService
             app(AccountService::class)->initiateTransfer($feeAmount);
 
             $reference = generateReference('ADMIN', 'transactions');
-            $user->createTransaction(TransactionTitle::ADMIN_CHARGE->value, $feeAmount, 'DR', $reference, 'Admin charge');
+            $user->createTransaction(TransactionTitle::ADMIN_CHARGE->value, $feeAmount, 'DR', $reference, 'You have received an admin charge.');
         });
     }
 
@@ -93,7 +93,7 @@ class ChargeService
             app(AccountService::class)->initiateTransfer($feeAmount);
 
             $reference = generateReference('VAT', 'transactions');
-            $user->createTransaction(TransactionTitle::VAT_CHARGE->value, $feeAmount, 'DR', $reference, 'VAT charge');
+            $user->createTransaction(TransactionTitle::VAT_CHARGE->value, $feeAmount, 'DR', $reference, 'You have received a VAT charge.');
         });
     }
 
