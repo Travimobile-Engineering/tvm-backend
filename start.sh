2318486a384b58deef431f2201226fd329de7c7d
Emmylong1 <emmanuelibok505@gmail.com>
2026-06-03 13:00:30 +0100

    fix: start php-fpm by running nightwatch:agent in background

--- start.sh
+++ start.sh
@@ -18,7 +18,7 @@ echo "🚀 Starting queue worker and reverb..."
 php artisan queue:work rabbitmq &    # background
 php artisan reverb:start &  # background
 php artisan schedule:work &  # background
-php artisan nightwatch:agent # background
+php artisan nightwatch:agent & # background
 
 # Start php-fpm in the foreground so the container doesn't exit
 echo "📦 Starting php-fpm..."
